349d4d78835c6e18f07649878d1eb24255ee3bc7
Samuel Novaes <samuel.novaes.96@gmail.com>
2017-12-10 20:44:30 -0200

    #

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let cgi = require('cgi')
 let path = require('path')
 let walk = require('walk')
 let fs = require('fs')
+let get_ip = require('ipware')().get_ip
 let app = express()
 let dir = process.cwd()
 let walker = walk.walk(dir)
@@ -27,12 +28,17 @@ fs.access(staticDir, fs.constants.R_OK, err => {
 	}
 })
 
+console.log('Creating routes')
+console.log()
+
 walker.on('file', (root, file, next) => {
 	if(root != path.join(dir, 'src') && root != staticDir){
 		let filename = path.parse(file.name).name
 		let route = `${root.replace(new RegExp(`^${dir}`), '')}/${filename == 'index' ? '' : filename}`
 		route = route.replace(/_/g, ':')
+		console.log(route)
 		app.all(route, (req, res, next) => {
+			console.log(req.method, req.path, `from ${get_ip(req).clientIp}`)
 			process.env.REQUEST = JSON.stringify({
 				body: req.body,
 				params: req.params,
@@ -48,6 +54,8 @@ walker.on('file', (root, file, next) => {
 
 walker.on('end', () => {
 	app.listen(port, () => {
+		console.log()
 		console.log(`Server running on port ${port}`)
+		console.log()
 	})
 })
